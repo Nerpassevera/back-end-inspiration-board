f04ec39b5cda25cab622afd32abc9b294f045845
Tatiana Trofimova <nerpassevera@gmail.com>
2024-12-22 12:47:05 -0800

    Updates Card model

--- app/models/card.py
+++ app/models/card.py
@@ -1,17 +1,14 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey
 from app.db import db
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from .board import Board
+
 class Card(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str] = mapped_column(String(255))
-    likes_count: Mapped[int] # dont forget to set defalt to 0
+    likes_count: Mapped[int] = mapped_column(Integer, default=0)
     board_id: Mapped[int] = mapped_column(ForeignKey("board.id"))
     board: Mapped["Board"] = relationship(back_populates="cards")
-
 
     def to_dict(self):
         return {
@@ -19,8 +16,8 @@ class Card(db.Model):
             "message": self.message,
             "likes_count": self.likes_count,
             "board_id": self.board_id
-    }
+        }
 
     @classmethod
     def from_dict(cls, data):
-        return Card(message=data["message"], likes_count=0, board_id=1)
+        return Card(message=data["message"], board_id=data["board_id"])
