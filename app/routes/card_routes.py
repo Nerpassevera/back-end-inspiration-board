9db3c8cdd9838f54bfab4d3adbb5f2d31c39e9f5
Tatiana Trofimova <nerpassevera@gmail.com>
2024-12-22 13:16:34 -0800

    Formatting corrections in card_routes

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -4,22 +4,26 @@ from app.models.card import Card
 
 bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
 
+# This route was asked by FE team but may be not useful
+# @bp.post("/", strict_slashes=False)
+# def create_card():
+#     return create_class_instance(Card, request, ["message"])
 
-@bp.post("/", strict_slashes=False)
-def create_card():
-    return create_class_instance(Card, request, ["message"])
 
 @bp.get('/')
 def get_all_cards():
     return get_all_instances(Card, request.args)
 
+
 @bp.get('/<card_id>')
 def get_one_card(card_id):
     return get_one_instance(Card, card_id)
 
+
 @bp.patch('/<card_id>')
 def update_card(card_id):
     return update_instance(Card, card_id, request)
+
 
 @bp.delete("/<card_id>", strict_slashes=False)
 def delete_card(card_id):
