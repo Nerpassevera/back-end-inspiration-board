39720a7788b686f91ed21ac900235e3a7b041539
Tatiana Trofimova <nerpassevera@gmail.com>
2025-01-02 13:38:25 -0800

    Removes redundant functionality from card_routes

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -5,23 +5,11 @@ from app.models.board import Board
 
 bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
 
-#  Ask FE team if this route is needed. If yes - add it to README
-@bp.get('/')
-def get_all_cards():
-    return get_all_instances(Card, request.args)
-
 
 @bp.get('/<card_id>')
 def get_one_card(card_id):
     return get_one_instance(Card, card_id)
 
-# # added this card... goes with 
-# @bp.patch("/<board_id>/cards")
-# def create_card_for_board(board_id):
-#     validate_model(Board, board_id)  
-#     response_data, status_code = create_class_instance(Card, request, ["message", "owner"], {"board_id": board_id})
-#     send_card_created_message(response_data["card"]["message"])
-#     return response_data, status_code
 
 @bp.patch('/<card_id>')
 def update_card(card_id):
